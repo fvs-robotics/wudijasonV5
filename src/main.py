34143d4fc7757c5c55100cf83f327ec73b7d6caa
iamjason-ctrl <jason1231232023@outlook.com>
2024-11-13 17:05:09 -0700

    first commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       xujiakai                                                     #
+# 	Created:      2024/11/13 17:04:20                                          #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
